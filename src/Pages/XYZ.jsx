e63209775b0ba106060781b31f1c9e6a95b5618b
TheCharm2003 <chirayuagrawal454@gmail.com>
2024-06-27 19:50:57 +0530

    Revert "trial 1"
    This reverts commit 791c9cf451ac4e87b10e00355e6a7584738c533a.

--- src/Pages/XYZ.jsx
+++ src/Pages/XYZ.jsx
@@ -15,7 +15,10 @@ function Main() {
                 <Row>
 
                     <Col sm="6"> 
+
+
                     
+
                         <Form.Group as={Row} className="mb-3" controlId="formPlaintextPassword">
                             <Form.Label column sm="2">
                                 Form No.
