2778b4a173e265a737cc76fd61a0bb0bc0125693
UchitChavda <uchitchavda1211@gmail.com>
2024-06-28 18:45:18 +0530

    Add Vehicle Form Added

--- src/Pages/ViewTransporter.jsx
+++ src/Pages/ViewTransporter.jsx
@@ -1,11 +1,58 @@
 import React from 'react'
 import { Card } from 'react-bootstrap'
+import Button from 'react-bootstrap/Button';
+import Modal from 'react-bootstrap/Modal';
+import { useState } from 'react';
+import Form from 'react-bootstrap/Form';
+import Col from 'react-bootstrap/Col';
+import Row from 'react-bootstrap/Row';
 
 function ViewTransporter() {
+
+
+    const [show, setShow] = useState(false);
+
+    const handleClose = () => setShow(false);
+    const handleShow = () => setShow(true);
+    const handleAdd = () => {
+        setShow(false);
+        alert("Vehicle Added Successfully")
+    }
+
+
     return (
         <div style={{ marginTop: "1.5rem" }}>
             <Card>
-                <h1>ViewTransporter</h1>
+                <Button variant="primary" onClick={handleShow}>
+                    Add Vehicle
+                </Button>
+
+                <Modal
+                    show={show}
+                    onHide={handleClose}
+                    backdrop="static"
+                    keyboard={false}
+                >
+                    <Modal.Header closeButton>
+                        <Modal.Title>Add Vehicle</Modal.Title>
+                    </Modal.Header>
+                    <Modal.Body>
+                        <Form.Group as={Row} className="mb-3" controlId="formPlaintextPassword">
+                            <Form.Label column sm="3">
+                                Vehicle No.
+                            </Form.Label>
+                            <Col sm="9">
+                                <Form.Control type="text" placeholder="Vehicle No." />
+                            </Col>
+                        </Form.Group>
+                    </Modal.Body>
+                    <Modal.Footer>
+                        <Button variant="danger" onClick={handleClose}>
+                            Cancel
+                        </Button>
+                        <Button variant="primary" onClick={handleAdd}>Add</Button>
+                    </Modal.Footer>
+                </Modal>
             </Card>
         </div>
     )
